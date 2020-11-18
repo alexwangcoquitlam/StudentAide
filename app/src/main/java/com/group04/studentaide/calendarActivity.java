86866ac51a01a31ff854d6f5c64eb720f00e404f
alexwangcoquitlam <72105768+alexwangcoquitlam@users.noreply.github.com>
2020-11-17 17:23:55 -0800

    Add files via upload

--- app/src/main/java/com/group04/studentaide/calendarActivity.java
+++ app/src/main/java/com/group04/studentaide/calendarActivity.java
@@ -11,18 +11,38 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class calendarActivity extends AppCompatActivity {
 
-    private TextView dateDisplay;
+    private TextView eventDisplay, eventDisplay2, eventDisplay3;
     private EditText event;
     private CalendarView calendarDisplay;
     private Button addEvent;
     private long dateOccur;
+    private String uid;
+    private String date;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     //https://medium.com/@Patel_Prashant_/android-custom-calendar-with-events-fa48dfca8257
 
@@ -34,22 +54,104 @@ public class calendarActivity extends AppCompatActivity {
         event = findViewById(R.id.eventName);
         addEvent = findViewById(R.id.addEvent);
         calendarDisplay = findViewById(R.id.calendarView);
-        dateDisplay = findViewById(R.id.dateTestLabel);
+        eventDisplay = findViewById(R.id.eventLabel);
+        eventDisplay2 = findViewById(R.id.eventLabel1);
+        eventDisplay3 = findViewById(R.id.eventLabel2);
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        String date = sdf.format(calendarDisplay.getDate());
-        dateDisplay.setText(date);
+        date = sdf.format(calendarDisplay.getDate());
+        dateOccur = calendarDisplay.getDate();
+        if (user != null) {
+            uid = user.getUid();
+        }
+
+        db.collection("Events")
+                .whereEqualTo("User_ID", uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<String> eventNames = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String documentDay = (String) document.get("Date");
+                                if(documentDay.equals(date)) {
+                                    String eventName = (String) document.get("Event_Name");
+                                    eventNames.add(eventName);
+                                }
+                                else{}
+                            }
+                            Log.d("listCreated", eventNames.toString());
+                            int listSize = eventNames.size();
+                            if (listSize == 0){}
+                            else if (listSize == 1){
+                                eventDisplay.setText(eventNames.get(0));
+                            }
+                            else if (listSize == 2){
+                                eventDisplay.setText(eventNames.get(0));
+                                eventDisplay2.setText(eventNames.get(1));
+                            }
+                            else if (listSize >= 3){
+                                eventDisplay.setText(eventNames.get(0));
+                                eventDisplay2.setText(eventNames.get(1));
+                                eventDisplay3.setText(eventNames.get(2));
+                            }
+                        } else {
+                            Log.v("listCreateFailed", "Error occurred when getting data from Firebase.");
+                        }
+                    }
+                });
 
         calendarDisplay.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             //show the selected date as a toast
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
-                dateDisplay.setText(day+"/"+(month+1)+"/"+year);
                 Calendar c = Calendar.getInstance();
                 c.set(year, month, day);
                 dateOccur = c.getTimeInMillis(); //this is what you want to use later
+                date = sdf.format(dateOccur);
+
+                db.collection("Events")
+                        .whereEqualTo("User_ID", uid)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    List<String> eventNames = new ArrayList<>();
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        String documentDay = (String) document.get("Date");
+                                        if(documentDay.equals(date)) {
+                                            String eventName = (String) document.get("Event_Name");
+                                            eventNames.add(eventName);
+                                        }
+                                        else{}
+                                    }
+                                    Log.d("listCreated", eventNames.toString());
+                                    int listSize = eventNames.size();
+                                    if (listSize == 0){}
+                                    else if (listSize == 1){
+                                        eventDisplay.setText(eventNames.get(0));
+                                    }
+                                    else if (listSize == 2){
+                                        eventDisplay.setText(eventNames.get(0));
+                                        eventDisplay2.setText(eventNames.get(1));
+                                    }
+                                    else if (listSize >= 3){
+                                        eventDisplay.setText(eventNames.get(0));
+                                        eventDisplay2.setText(eventNames.get(1));
+                                        eventDisplay3.setText(eventNames.get(2));
+                                    }
+                                } else {
+                                    Log.v("listCreateFailed", "Error occurred when getting data from Firebase.");
+                                }
+                            }
+                        });
+
+
             }
         });
+
         addEvent.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -73,6 +175,33 @@ public class calendarActivity extends AppCompatActivity {
             else{
                 Toast.makeText(calendarActivity.this, "There is no app that can support this action.", Toast.LENGTH_SHORT).show();
             }
+
+            if(user != null) {
+                uid = user.getUid();
+                String eventName = event.getText().toString().trim();
+                String name = user.getDisplayName().toLowerCase();
+                Map<String, Object> Events = new HashMap<>();
+                Events.put("User_Name", name);
+                Events.put("User_ID", uid);
+                Events.put("Event_Name", eventName);
+                Events.put("Date", date);
+
+                db.collection("Events")
+                        .add(Events)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("eventAdded", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("eventAddFail", "Error adding document", e);
+                            }
+                        });
+            }
+            else{}
         }
         else{
             Toast.makeText(calendarActivity.this, "Please specify an event.", Toast.LENGTH_SHORT).show();
