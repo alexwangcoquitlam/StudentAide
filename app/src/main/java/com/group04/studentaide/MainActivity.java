c484da202edf9d84474bf83208047675bb37646b
alexwangcoquitlam <72105768+alexwangcoquitlam@users.noreply.github.com>
2020-11-22 00:04:33 -0800

    Add files via upload

--- app/src/main/java/com/group04/studentaide/MainActivity.java
+++ app/src/main/java/com/group04/studentaide/MainActivity.java
@@ -2,27 +2,49 @@ package com.group04.studentaide;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button loginScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginScreen = findViewById(R.id.loginOpen);
+        loginScreen.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent loginGuest = new Intent(MainActivity.this, loginActivityGuest.class);
+                startActivity(loginGuest);
+            }
+        });
+
+        informationRetrieval infoRetrieve = informationRetrieval.getInstance();
+        String userDocID = infoRetrieve.getDocumentID();
+        Log.v("Loginn", "Test: " + userDocID);
+
     }
 
-  
-  
+
+
     //Yufeng: I'm not sure if View view needs to be passed into these functions because when the functions are called
     //and the new Intent is created, startActivity will open the class that is associated with the Intent
-  
-    public void loginScreen(View view){
-        Intent login = new Intent(this, loginActivity.class);
-        startActivity(login);
-    }
 
     public void calendarScreen(View view){
         Intent calendar = new Intent(this, calendarActivity.class);
