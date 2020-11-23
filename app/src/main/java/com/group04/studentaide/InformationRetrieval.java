a76198ab216903a7e6f571462b6def52b7cb01a0
Hareye <40867693+Hareye@users.noreply.github.com>
2020-11-22 20:04:21 -0800

    Update InformationRetrieval.java

--- app/src/main/java/com/group04/studentaide/InformationRetrieval.java
+++ app/src/main/java/com/group04/studentaide/InformationRetrieval.java
@@ -12,6 +12,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InformationRetrieval {
 
     private static InformationRetrieval ourInstance = null;
@@ -49,6 +52,38 @@ public class InformationRetrieval {
 
     }
 
+    public void updateID() {
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+
+            String uid = user.getUid();
+            db.collection("Students")
+                    .whereEqualTo("User_ID", uid)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d("documentIDGet", "Document ID is: " + document.getId());
+                                    studentDocumentID = document.getId();
+                                }
+                            } else {
+                                Log.v("signedinwrong", "you're signed into the wrong account for testing");
+                            }
+                        }
+                    });
+
+        } else {
+
+            studentDocumentID = null;
+
+        }
+
+    }
+
     public static InformationRetrieval getInstance() {
         if (ourInstance == null){
             ourInstance = new InformationRetrieval();
@@ -58,6 +93,7 @@ public class InformationRetrieval {
 
     public String getDocumentID() {
 
+        Log.v("Hareye", studentDocumentID);
         return studentDocumentID;
 
     }
