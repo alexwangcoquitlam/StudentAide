e102c15b464d526df561c7347d9b5754e908d7b9
Hareye <40867693+Hareye@users.noreply.github.com>
2020-11-22 17:21:59 -0800

    Update serverURL.java

--- app/src/main/java/com/group04/studentaide/serverURL.java
+++ app/src/main/java/com/group04/studentaide/serverURL.java
@@ -1,6 +1,6 @@
 package com.group04.studentaide;
 
-public class serverURL {
+public class ServerURL {
 
     //ROOT_URL missing the end, or is format http://studentaide.canadacentral.cloudapp.azure.com/register ??
     public static final String ROOT_URL = "http://studentaide.canadacentral.cloudapp.azure.com/";
