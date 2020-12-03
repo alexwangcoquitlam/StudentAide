7548d107065601c5b7f3e06b88e78771a88dd9ad
yufengl <yufeng999@hotmail.com>
2020-12-03 15:03:08 -0800

    Display Student courses in a separate activity

--- app/src/main/java/com/group04/studentaide/QuizCreate.java
+++ app/src/main/java/com/group04/studentaide/QuizCreate.java
@@ -5,42 +5,41 @@ Written By: Yufeng Luo
 */
 
 import android.content.Intent;
+
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class QuizCreate extends AppCompatActivity {
 
     private Button quizQuestionsCreateOpen;
-    private Spinner dueDay, dueMonth, dueYear, dueHour, dueMinute, courseName;
+    private EditText quizName;
+    private Spinner dueDay, dueMonth, dueYear, dueHour, dueMinute;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_creation);
 
+        dueDay = findViewById(R.id.dueDateDay3);
+        dueMonth = findViewById(R.id.dueDateMonth);
+        dueYear = findViewById(R.id.dueDateYear);
+        dueHour = findViewById(R.id.dueDateHour);
+        dueMinute = findViewById(R.id.dueDateMinute);
+        quizName = findViewById(R.id.quizName);
+
         quizQuestionsCreateOpen = findViewById(R.id.quizQuestionsActivityOpen);
+
+        QuizQuestions newQuiz = new QuizQuestions();
 
         quizQuestionsCreateOpen.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent quizQuestionsCreate = new Intent(QuizCreate.this, QuizQuestionsCreate.class);
+                quizQuestionsCreate.putExtra("quiz", newQuiz);
                 startActivity(quizQuestionsCreate);
             }
         });
